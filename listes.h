7878c3c55a7e3012f8d21e5cfb4d1e638903d6e0
Delannma <43544882+Delannma@users.noreply.github.com>
2018-09-25 17:21:32 +0200

    Update listes.h

--- listes.h
+++ listes.h
@@ -1,5 +1,5 @@
-#ifndef _ANALYSE_LEXICAL_H
-#define _ANALYSE_LEXICAL_H  
+#ifndef _LISTES_H
+#define _LISTES_H  
                                          
 
 
@@ -8,15 +8,13 @@
 // fonctions basiques pour les listes
 
 LISTE_LEXEME creer_liste( void );
-int est_vide( LISTE_LEXEME );
+int est_vide( LISTE_LEXEME L);
 LISTE_LEXEME ajout_tete( LEXEME lexeme, LISTE_LEXEME L);
 
 // les listes doivent etres non vides
 LEXEME obtenir_tete(LISTE_LEXEME L);
 LISTE_LEXEME supp_tete( LISTE_LEXEME L);
-void visualiser( LISTE_LEXEME L);
 int compter( LISTE_LEXEME L);
-
 
 
 
